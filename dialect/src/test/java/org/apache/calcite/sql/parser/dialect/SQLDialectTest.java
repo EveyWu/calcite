32c1f006f5221e281731d5a54762fb5e04400ccb
yiwenwu <yiwenwu@tencent.com>
2021-03-15 11:27:33 +0800

    update sql dialect

--- dialect/src/test/java/org/apache/calcite/sql/parser/dialect/SQLDialectTest.java
+++ dialect/src/test/java/org/apache/calcite/sql/parser/dialect/SQLDialectTest.java
@@ -51,8 +51,15 @@ public class SQLDialectTest {
   void simpleSQL() throws SqlParseException {
     /*String sql =
         "insert into newstu select id,name,age FROM (select id,name,age FROM stu) T left join T2 on T.id = T2.rid where age<20";*/
+    //外表
+    /*String sql =
+        "CREATE EXTERNAL TABLE list_bucket_multiple (col1 STRING, col2 int, col3 STRING)";*/
+    //临时表
+    //String sql = "CREATE TEMPORARY TABLE list_bucket_multiple (col1 STRING, col2 int, col3 STRING)";
+
+    //支持create comment
     String sql =
-        "CREATE EXTERNAL TABLE list_bucket_multiple (col1 STRING, col2 int, col3 STRING)";
+        "CREATE EXTERNAL TABLE page_view(viewTime INT) COMMENT 'This is the staging page view table'";
 
     // 解析配置
     //SqlParser.Config config = SqlParser.configBuilder().setLex(Lex.MYSQL).build();
